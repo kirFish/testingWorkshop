5590a5950824a33af2f2af1400545297d0986051
Kyrylo Fishchuk <kir.fishchuk2014@gmail.com>
2025-02-27 15:20:38 +0100

    Initialized the project, added license and basic sceleton

---
+++ src/test/MainTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class MainTest {
+
+    @BeforeClass
+    public static void beforeClass(){
+
+    }
+
+    @Test
+    public void mainTest(){}
+
+    @AfterClass
+    public static void afterClass(){
+
+    }
+}
